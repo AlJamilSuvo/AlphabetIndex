b373b53456f02cb9c60a36b06d17f1683a0d741c
Al-Jamil Suvo <jamil.suvo@revesoft.com>
2020-02-24 16:21:20 +0600

    added many config

--- app/src/main/java/al/jamil/suvo/alphabetindex/AlphabetIndexView.java
+++ app/src/main/java/al/jamil/suvo/alphabetindex/AlphabetIndexView.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -12,9 +12,9 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
-
 
 
 import java.util.ArrayList;
@@ -36,15 +36,24 @@ public class AlphabetIndexView extends RelativeLayout {
         super(context, attrs);
         binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.alphabet_index_list, this, true);
         TypedArray properties = context.obtainStyledAttributes(attrs, R.styleable.AlphabetIndexView);
-        int textColor = properties.getColor(R.styleable.AlphabetIndexView_box_text_color, Color.argb(255, 0, 0, 0));
-        binding.tvAlphabetIndicator.setTextColor(textColor);
-        boolean useCustomImage = properties.getBoolean(R.styleable.AlphabetIndexView_use_custom_image, false);
+        int bubbleColor = properties.getColor(R.styleable.AlphabetIndexView_bubble_color, Color.argb(255, 0, 0, 255));
+        int bubbleTextColor = properties.getColor(R.styleable.AlphabetIndexView_bubble_text_color, Color.argb(255, 255, 255, 255));
+        int scrollIndicatorColor = properties.getColor(R.styleable.AlphabetIndexView_scroll_indicator_color, Color.argb(255, 0, 0, 255));
+        int listAlphabetColor = properties.getColor(R.styleable.AlphabetIndexView_list_alphabet_text_color, Color.argb(255, 0, 0, 0));
+        boolean useCustomImage = properties.getBoolean(R.styleable.AlphabetIndexView_use_custom_bubble_image, false);
         if (useCustomImage) {
-            binding.alphabetIndicator.setBackground(properties.getDrawable(R.styleable.AlphabetIndexView_use_custom_image));
+            binding.alphabetIndicator.setBackground(properties.getDrawable(R.styleable.AlphabetIndexView_custom_bubble_image));
+        } else {
+            Drawable bubbleUnWrapped = context.getResources().getDrawable(R.drawable.ic_alphabet_indicator_bg);
+            Drawable bubbleWrappedDrawable = DrawableCompat.wrap(bubbleUnWrapped);
+            DrawableCompat.setTint(bubbleWrappedDrawable, bubbleColor);
+            binding.alphabetIndicator.setBackground(bubbleWrappedDrawable);
         }
+        binding.scrollIndicator.setColorFilter(scrollIndicatorColor);
+        binding.tvAlphabetIndicator.setTextColor(bubbleTextColor);
 
 
-        adapter = new AlphabetAdapter();
+        adapter = new AlphabetAdapter(listAlphabetColor);
         binding.alphabetList.setAdapter(adapter);
         binding.alphabetIndicator.setVisibility(GONE);
         this.alphabetIndexMap = new HashMap<>();
@@ -118,20 +127,29 @@ public class AlphabetIndexView extends RelativeLayout {
     class AlphabetViewHolder extends RecyclerView.ViewHolder {
         AlphabetIndexSingleItemBinding binding;
 
-        AlphabetViewHolder(AlphabetIndexSingleItemBinding binding) {
+        AlphabetViewHolder(AlphabetIndexSingleItemBinding binding, int textColor) {
             super(binding.getRoot());
             this.binding = binding;
+            binding.alphabetHighlighted.setTextColor(textColor);
+            binding.alphabetNormal.setTextColor(textColor);
         }
     }
 
     class AlphabetAdapter extends RecyclerView.Adapter<AlphabetViewHolder> {
+
+
+        int textColor;
+
+        public AlphabetAdapter(int textColor) {
+            this.textColor = textColor;
+        }
 
         @NonNull
         @Override
         public AlphabetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             AlphabetIndexSingleItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
                     R.layout.alphabet_index_single_item, parent, false);
-            return new AlphabetViewHolder(binding);
+            return new AlphabetViewHolder(binding, textColor);
         }
 
         @Override
